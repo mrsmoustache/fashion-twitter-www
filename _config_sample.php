6e84427092460adf8c0258e56106b51d339e250d
Dominic Espinosa <dominicespinosa@admins-MacBook-Pro.local>
2011-08-01 18:31:21 -0400

    fixed config sample

--- _config_sample.php
+++ _config_sample.php
@@ -7,18 +7,3 @@ $GA_ANALYTICS = true; //Turn on/off Google Analytics
 $WEB_ROOT = "/";
 
 ?>
-
-<!DOCTYPE html>
-
-<!-- 
-320 and Up boilerplate extension
-Author: Andy Clarke
-Version: 0.9b
-URL: http://stuffandnonsense.co.uk/projects/320andup 
--->
-
-<!--[if IEMobile 7 ]><html class="no-js iem7" manifest="default.appcache?v=1"><![endif]-->
-<!--[if lt IE 7 ]><html class="no-js ie6" lang="en"><![endif]-->
-<!--[if IE 7 ]><html class="no-js ie7" lang="en"><![endif]-->
-<!--[if IE 8 ]><html class="no-js ie8" lang="en"><![endif]-->
-<!--[if (gte IE 9)|(gt IEMobile 7)|!(IEMobile)|!(IE)]><!--><html class="no-js" manifest="default.appcache?v=1" lang="en"><!--<![endif]-->
